3b9e8232dff5cd885c4ce40b2017856e120f415f
Supeng (Soho) Li <superlisohou@gmail.com>
2021-02-14 21:29:57 +0900

    the initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# todo-express
+
+A simple todo express application implemented by Express.js
+
+## Installation
+```sh
+$ npm install .
+```
+
+## Usage
+### From the command line
+```sh
+$ npm start
+```
---
+++ app.js
@@ -0,0 +1,52 @@
+"use strict";
+const express = require("express");
+const bodyParser = require('body-parser')
+
+let todos = [
+    {id: 1, title: "script", completed: false},
+    {id: 2, title: "draft", completed: true},
+]
+let id = 2;
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Get a list of ToDo
+app.get("/api/todos", (req, res) => {
+    if(!req.query.completed) {
+        res.json(todos)
+    }
+    else {
+        const completed = req.query.completed === "true";
+        res.json(todos.filter(todo => todo.completed === completed));
+    }
+});
+
+// Add a new ToDo
+app.post("/api/todos", (req, res, next) => {
+    const {title} = req.body;
+    if(!title || typeof title !== "string") {
+        const err = new Error("Title is required");
+        err.statusCode = 400;
+        return next(err);
+    }
+    else {
+        // Create a new ToDo
+        const todo = {id: id += 1, title, completed: false};
+        todos.push(todo);
+        res.status(201).json(todo);
+    }
+}
+)
+
+// Error Handler Middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.statusCode || 500).json({error: err.message});
+})
+app.listen(3000);
